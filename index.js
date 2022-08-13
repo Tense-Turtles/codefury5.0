647943ba9e98a7a63e50686e9050d46e65206c86
Shell Raiser <ksshailesh05@gmail.com>
2022-08-13 10:08:21 +0530

    add path packageand implement it

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var express = require('express');
 var cors = require('cors')
 var app = express();
 app.use(cors())
+var path = require('path');
+global.appRoot = path.resolve(__dirname);
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + './index.html');
-    
-
+    res.sendFile(__dirname + '/index.html');
 })
 
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -13,6 +13,6 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, function() {
+app.listen(port, function () {
     console.log('Your app is listening on http://localhost:' + port)
 });
